238742a088b292836deb97c4cf515b7f1a76a1d0
Wojtek Kaźmierczak <44867612+wordydeer@users.noreply.github.com>
2020-10-28 08:51:14 +0100

    stage3

--- insert.php
+++ insert.php
@@ -1,8 +1,8 @@
 <?php
-     $conn = new mysqli("127.0.0.1", "root", "", "bibl");
+     $conn = new mysqli("sql7.freemysqlhosting.net", "sql7373145", "neAKxXba6X", "sql7373145");
      $autor = $_POST['name'];
      $tytul = $_POST['tytul'];
-     $sql = "INSERT INTO lib_autor VALUES ($autor); INSERT INTO lib_tytul VALUES ($tytul)";
+     $sql = "INSERT INTO lib_autor VALUES ($autor); INSERT INTO lib_tytul VALUES ($tytul);";
      $result = mysqli_query($conn, $sql);
      header("Location: index.php");
 ?>
