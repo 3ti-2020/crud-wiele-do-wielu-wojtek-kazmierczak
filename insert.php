1b88d3106cee9f34c5c361341557f62dda92cdbb
Tomjand <domagala.tomasz@gmail.com>
2020-11-03 11:02:02 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,7 +2,14 @@
      $conn = new mysqli("sql7.freemysqlhosting.net", "sql7373145", "neAKxXba6X", "sql7373145");
      $autor = $_POST['name'];
      $tytul = $_POST['tytul'];
-     $sql = "INSERT INTO lib_autor VALUES (NULL, $autor); INSERT INTO lib_tytul VALUES (NULL, $tytul);";
+
+     $sql1 = "INSERT INTO lib_autor VALUES (NULL, $autor)"; 
+          $last_idlib_autor = $conn->insert_id;
+     $sql2 = "INSERT INTO lib_tytul VALUES (NULL, $tytul)";
+         $last_idlib_tytul = $conn->insert_id;     
+   
+     $sql3 = "INSERT INTO krzyzowej VALUES (NULL, $last_idlib_tytul,$last_idlib_autor)";
+
      $result = mysqli_query($conn, $sql);
      header("Location: index.php");
 ?>
