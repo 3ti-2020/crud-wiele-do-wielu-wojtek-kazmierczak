78b012fc11875e2f1205a08ef63c5762bbf6ac3a
Wojtek Kaźmierczak <44867612+wordydeer@users.noreply.github.com>
2020-11-03 11:18:54 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -6,10 +6,9 @@
      $sql1 = "INSERT INTO lib_autor VALUES (NULL, $autor)"; 
           $last_idlib_autor = $conn->insert_id;
      $sql2 = "INSERT INTO lib_tytul VALUES (NULL, $tytul)";
-         $last_idlib_tytul = $conn->insert_id;     
+          $last_idlib_tytul = $conn->insert_id;     
    
-     $sql3 = "INSERT INTO lib_autor_tytul VALUES (NULL, $last_idlib_tytul,$last_idlib_autor)";
+     $sql3 = "INSERT INTO lib_autor_tytul VALUES (NULL, $last_idlib_autor,$last_idlib_tytul)";
 
      $result = mysqli_query($conn, $sql);
      header("Location: index.php");
-?>
