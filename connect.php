39b6c4cbbe0f4e76b36858ad0c3dc2cfc97182ab
Wojtek Kaźmierczak <44867612+wordydeer@users.noreply.github.com>
2020-11-10 14:30:26 +0100

    safe

--- connect.php
+++ connect.php
@@ -1,4 +1,3 @@
 <?php
-    $conn = new mysqli("mysql-wojtini.alwaysdata.net", "wojtini", "fdbfre21", "wojtini_bibl");
-    // $conn = new mysqli("127.0.0.1", "root", "", "bibl");
+    $conn = new mysqli("mysql-wojtini.alwaysdata.net", "wojtini", "Haslo@!37", "wojtini_bibl");
 ?>
