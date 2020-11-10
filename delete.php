4db992daee8b56ea3c7471a70936285c353dd702
Wojtek Kaźmierczak <44867612+wordydeer@users.noreply.github.com>
2020-11-10 11:06:08 +0100

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,6 +1,7 @@
 <?php
     require_once("connect.php");
     $del = $_POST['id_del'];
-
-    
+    $sql = 'DELETE FROM lib_autor_tytul WHERE id_autor_tytul=$del';
+    mysqli_query($conn, $sql);
+    header("Location:index.php");
 ?>
